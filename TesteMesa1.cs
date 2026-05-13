0e4b4e0a6959847e5068a35b5a038b4fa310b1ce
Kamile Alves da Silva <kamilealvss@gmail.com>
2026-05-13 14:26:01 -0300

    Add files via upload

---
+++ TesteMesa1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace teste01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        //a= 10;
+        //b= 20;
+        //c = (a + b) / 2
+        //c = c-40;
+        //v[3] =a+b+c
+
+      const int a=10;
+      const int b=20;
+      int c;
+
+      c = (a + b) / 2;
+      c = c - 40;
+
+      int[] v= new int[4];
+      v[3] = a+b+c;
+
+      Console.WriteLine(v[3]);
+ 
+    }
+  }
+}
