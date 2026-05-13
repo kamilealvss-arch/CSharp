1e245d7a188713e256b6bf297d83a8d622d60f88
Kamile Alves da Silva <kamilealvss@gmail.com>
2026-05-13 15:32:08 -0300

    Update TesteMesa2.cs
    Updated the loop to print values of the array 'v' and corrected the increment of 'a'.

--- TesteMesa2.cs
+++ TesteMesa2.cs
@@ -5,25 +5,26 @@ namespace teste02
   class Program
   {
     static void Main(string[] args)
-    {
+     {
 
-           //a= 2;
-            //while (a<6)  {
-            //v[a] = 10 * a;
-            //a += 1; } 
+    //a= 2;
+    //while (a<6)  {
+    //v[a] = 10 * a;
+    //a += 1;}
+    
+        int[] v = new int[6];
+        int a = 2;
 
-            int[] v = new int[6];
-            int a=2;
-
-            while (a<6)
-            {
-                v[a] = 10 * a;
-                a +=2;
-            }
-
-            for (int i = 0; i v.Length; i++)
-            Console.WriteLine ($"v[{i}] = {v[i]}");
-            
+        while (a < 6)
+        {
+            v[a] = 10 * a;
+            Console.WriteLine(v[a]);
+            a += 1;
         }
     }
+  }
 }
+
+    
+    
+  
