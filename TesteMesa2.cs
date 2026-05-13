dff562ed69dda14f2f219703c47a4c751ca14499
Kamile Alves da Silva <kamilealvss@gmail.com>
2026-05-13 15:31:22 -0300

    Add files via upload

---
+++ TesteMesa2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace teste02
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+           //a= 2;
+            //while (a<6)  {
+            //v[a] = 10 * a;
+            //a += 1; } 
+
+            int[] v = new int[6];
+            int a=2;
+
+            while (a<6)
+            {
+                v[a] = 10 * a;
+                a +=2;
+            }
+
+            for (int i = 0; i v.Length; i++)
+            Console.WriteLine ($"v[{i}] = {v[i]}");
+            
+        }
+    }
+}
