d28fe3d039e4db2e9d6e0dd02864f617cba770be
Kamile Alves da Silva <kamilealvss@gmail.com>
2026-05-20 17:26:27 -0300

    Add files via upload

---
+++ TesteMesa4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Rendimento
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        double p;
+        double i;
+        double f;
+        double n;
+        string tipoTempo;
+
+        Console.WriteLine("Digite o valor que deseja invetir:");
+        p = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o valor do juros:");
+        i = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Quer Calcular o tempo em meses ou anos? (meses/anos)");
+        tipoTempo = Console.ReadLine()?.ToLower() ?? "";
+
+        Console.WriteLine("Digite o valor do tempo (período):");
+        n = Convert.ToDouble(Console.ReadLine());
+
+         if (tipoTempo == "anos")
+            {
+                n = n * 12;
+            }
+
+            f = p * Math.Pow(1 + (i/100), n);
+
+             Console.WriteLine($"O valor final é de: {f:C}");
+
+    }
+  }
+}
